b113b79890074f62afe48de5a70f761f0ea7b0a6
EleazarBiida <eleazar717@icloud.com>
2024-10-22 16:47:27 -0600

    Se agregan estilos para adaptar el la vista del menú en un celular

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -4,21 +4,22 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
   usuario: string = localStorage.getItem('usuario'); 
+  menuOpen: boolean = false;
 
-  constructor(
-    private router: Router
-  ) {
+  constructor(private router: Router) {}
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
   }
 
   cerrarSesion() {
     localStorage.removeItem('token');
     localStorage.removeItem('usuario'); 
     localStorage.removeItem('usuario_id'); 
-		this.router.navigate([`/login`]);
+    this.router.navigate(['/login']);
   }  
 }
