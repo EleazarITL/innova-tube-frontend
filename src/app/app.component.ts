0af264ea6b90270b3cd68a9d75df7e3317a18276
EleazarBiida <eleazar717@icloud.com>
2024-10-22 16:33:58 -0600

    Se modifico el metodo que oculta el menú en el /login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'InnovaTube';
-
   showMenu = true;
 
-  constructor(private router: Router) {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
       console.log('Navigated to:', event.url);
-      this.showMenu = !event.url.includes('/login');
+      this.showMenu = !(event.url === '/login' || event.url === '/' || event.url.startsWith('/login/'));
     });
+    
+    this.showMenu = !this.router.url.includes('/login');
   }
 }
